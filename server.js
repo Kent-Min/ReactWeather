a322bae7e3b9dfa19f1f39d8c6d8628b95b67b8e
Kent <minwenyan@gmail.com>
2016-08-02 21:03:23 -0500

    fixing local host

--- server.js
+++ server.js
@@ -6,15 +6,16 @@ var app = express ();
 const PORT = process.env.PORT || 3000;
 
 
-//mid-ware
-// app.use(function (req, res, next) {
-//   if (req.headers['x-forwarded-proto'] === 'http') {
-//     next();
-//   } else {
-//     res.redirect('http://'+ req.hostname + req.url);
-//   }
-//
-// });
+// mid-ware
+//this fixed the local host; only change was http -> https
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://'+ req.hostname + req.url);
+  } else {
+    next();
+  }
+
+});
 
 app.use(express.static('public'));
 
